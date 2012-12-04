7e07b732682e59c968a0b0fce38c4e73b846bc5b
zhangwei <zhangwei@zhangwei-Aspire-M3970.(none)>
2012-12-04 13:48:15 +0800

    modified:   gendata.m

--- datasets/gendata.m
+++ datasets/gendata.m
@@ -1,4 +1,4 @@
-function data = gendata(numdata, numclass, dim)
+function data = gendata(numdata, numclass, biasmean, dim)
 %
 %   GENDATA generates data accroding to different Gaussian
 %       functions.
@@ -10,17 +10,44 @@ function data = gendata(numdata, numclass, dim)
 %           (3)length = 1 and numdata == 0, randly choose the
 %           number of data.
 %       numclass --> total number of class of the data. 
+%       biasmean --> mean value of Gaussian distribution.
+%           In this test, bias mean is set in advance.
 %       dim --> number of dimensions of data.
 %
 %   Date: 12/4/2012
 
-% components
+
+% components setting
+N = 200;    % maximum of data points
+
 data.dim = dim;
 if length(numdata) == 1 & numdata ~= 0,
     data.numclass = numclass;
     data.numdata = repmat(numdata, 1, data.numclass);
+    data.biasmean = biasmean;
 elseif length(numdata) > 1,
     data.numclass = numclass;
+    data.numdata = numdata;
+    data.biasmean = biasmean;
+elseif length(numdata) == 1 & numdata == 0,
+    data.numclass = numclass;
+    for ii=1:data.numclass,
+        data.numdata(ii) = unidrnd(N);
+    end
+    data.biasmean = biasmean;
 end
 
-data.ss = repmat(0.0, sum(data.numdata), )
+% data space allocation
+data.ss = repmat(0.0, sum(data.numdata), data.dim);
+data.dataclass = repmat(0, sum(data.numdata), 1);
+
+% data generation
+data.ss(1:data.numdata(1)) = normrnd(0, 1, data.numdata(1), 2)...
+    + repmat(data.biasmean(1,:), data.numdata(1), 1);
+data.cc(1:data.numdata(1)) = 1;
+for ii =2:data.numclass,
+    data.ss(data.numdata(1)+1:data.numdata(2)) = normrnd(0, 1,...
+        data.numdata(ii), 2) + repmat(data.biasmean(ii,), data.numdata(ii),1);
+    data.cc(data.numdata(1)+1:data.numdata(2)) = ii;
+end
+
